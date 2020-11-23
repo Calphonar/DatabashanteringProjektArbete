5d994d3de7ba822474acbf43787aa1b5989f23f9
Calphonar <Richardzejak@hotmail.com>
2020-11-23 09:18:10 +0100

    Added Working Sort function between a-z and highest rating

--- Store/MainWindow.xaml.cs
+++ Store/MainWindow.xaml.cs
@@ -24,74 +24,6 @@ namespace Store
         public MainWindow()
         {
             InitializeComponent();
-
-            State.Movies = API.GetMovieSliceName(0, 30); ;
-            for (int y = 0; y < MovieGrid.RowDefinitions.Count; y++)
-            {
-                for (int x = 0; x < MovieGrid.ColumnDefinitions.Count; x++)
-                {
-                    int i = y * MovieGrid.ColumnDefinitions.Count + x;
-                    if (i < State.Movies.Count)
-                    {
-                        var movie = State.Movies[i];
-                        try
-                        {
-                            var text = new Label() { }; // variabel för texten
-                            text.Content = movie.Title; // Vad texten ska innehålla, i detta fall Movie.Title i databasen
-                            text.HorizontalAlignment = HorizontalAlignment.Center; // vart texten ska ligga horisontelt
-                            text.VerticalAlignment = VerticalAlignment.Top; // vart texten ska ligga vertikalt
-                            text.FontSize = 16;
-                            text.FontWeight = FontWeights.UltraBold;
-                            text.FontFamily = new FontFamily("Sans-Serif");
-                            text.Foreground = Brushes.White;
-
-                            var image = new Image() { };
-                            image.Cursor = Cursors.Hand; // om man håller över en bild blir det ett sånt pekfinger
-                            image.MouseUp += Image_MouseUp; // Om man klickar på en bilden skickas man ner till Image_MouseUp Metoden.
-                            image.HorizontalAlignment = HorizontalAlignment.Center;
-                            image.VerticalAlignment = VerticalAlignment.Center;
-                            image.Source = new BitmapImage(new Uri(movie.ImageURL)); // hämtar url från ImageURL i databasen till bilderna
-                            image.Height = 120;
-                            image.Margin = new Thickness(4, 4, 4, 4);
-
-                            var rating = new Label() { };
-                            rating.Content = movie.Rating + "/10 ★";
-                            rating.HorizontalAlignment = HorizontalAlignment.Center;
-                            rating.VerticalAlignment = VerticalAlignment.Bottom;
-                            rating.Foreground = Brushes.White;
-                            rating.Margin = new Thickness(-5);
-
-                            var genre = new Label() { };
-                            genre.Content = movie.Genre;
-                            genre.HorizontalAlignment = HorizontalAlignment.Center;
-                            genre.VerticalAlignment = VerticalAlignment.Bottom;
-                            genre.Foreground = Brushes.White;
-                            genre.Margin = new Thickness(15);
-
-
-                            MovieGrid.Children.Add(text); // säger till att texten ska tillhöra den gridden
-                            Grid.SetRow(text, y); // vilken grid i y
-                            Grid.SetColumn(text, x); // vilken grid i x
-                            MovieGrid.Children.Add(image);
-                            Grid.SetRow(image, y);
-                            Grid.SetColumn(image, x);
-                            MovieGrid.Children.Add(rating);
-                            Grid.SetRow(rating, y);
-                            Grid.SetColumn(rating, x);
-                            MovieGrid.Children.Add(genre);
-                            Grid.SetRow(genre, y);
-                            Grid.SetColumn(genre, x);
-                        }
-                        catch (Exception e) when
-                            (e is ArgumentNullException ||
-                             e is System.IO.FileNotFoundException ||
-                             e is UriFormatException)
-                        {
-                            continue;
-                        }
-                    }
-                }
-            }
         }
 
         private void Image_MouseUp(object sender, MouseButtonEventArgs e)
@@ -127,9 +59,6 @@ namespace Store
             switch (cmbSelect.SelectedItem.ToString().Split(new string[] { ": " }, StringSplitOptions.None).Last())
             {
                 case "Sort by name a-z":
-                    MovieGrid.Row.Clear();
-                    InitializeComponent();
-
                     State.Movies = API.GetMovieSliceName(0, 30);
                     for (int y = 0; y < MovieGrid.RowDefinitions.Count; y++)
                     {
@@ -202,7 +131,9 @@ namespace Store
                     //Handle for the second combobox
                     break;
                 case "Sort by highest rating":
-                   
+                    var next_window = new MainWindowByRating();
+                    next_window.Show();
+                    this.Close();
                     break;
                 case "Sort by lowest rating":
                     MovieGrid.Children.Clear();
